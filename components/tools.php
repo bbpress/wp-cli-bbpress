a0a653b6e9c04f7d90c2e1c621cdd4ce9ca82482
Renato Alves <espellcaste@gmail.com>
2017-10-01 20:19:49 -0300

    Tools: Added support for the wp bbp tools upgrade

--- components/tools.php
+++ components/tools.php
@@ -42,9 +42,61 @@ class BBPCLI_Tools extends BBPCLI_Component {
 		if ( function_exists( $repair ) ) {
 			$result = $repair();
 
-			WP_CLI::success( $result[1] );
+			if ( 0 === $result[0] ) {
+				WP_CLI::success( $result[1] );
+			} else {
+				WP_CLI::error( sprintf( 'Error: %s', $result[1] ) );
+			}
 		} else {
 			WP_CLI::error( 'There is no repair tool with that name.' );
+		}
+	}
+
+	/**
+	 * Upgrade.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--type=<type>]
+	 * : Name of the upgrade tool.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    wp bbp tools upgrade --type=user-engagements
+	 *    wp bbp tools upgrade --type=user-favorites
+	 *
+	 * @synopsis [--type=<type>]
+	 *
+	 * @since 1.0
+	 */
+	public function upgrade( $args, $assoc_args ) {
+		$defaults = array(
+			'type' => '',
+		);
+
+		$r = wp_parse_args( $assoc_args, $defaults );
+
+		// If no type added, bail it.
+		if ( empty( $r['type'] ) ) {
+			WP_CLI::error( 'You need to add a name of the upgrade tool.' );
+		}
+
+		// Convert to underscore.
+		$type = str_replace( '-', '_', $r['type'] );
+
+		// Repair function.
+		$upgrade = 'bbp_admin_upgrade_' . $type;
+
+		if ( function_exists( $upgrade ) ) {
+			$result = $upgrade();
+
+			if ( 0 === $result[0] ) {
+				WP_CLI::success( $result[1] );
+			} else {
+				WP_CLI::error( sprintf( 'Error: %s', $result[1] ) );
+			}
+		} else {
+			WP_CLI::error( 'There is no upgrade tool with that name.' );
 		}
 	}
 }

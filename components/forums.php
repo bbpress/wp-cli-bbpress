c857468e81936bf5c8c3b58068ec08c612637ed1
Renato Alves <espellcaste@gmail.com>
2017-10-30 23:10:20 -0300

    Forums: Added support for wp bbp forum open

--- components/forums.php
+++ components/forums.php
@@ -5,9 +5,35 @@
  * @since 1.0.0
  */
 class BBPCLI_Forums extends BBPCLI_Component {
+
+	/**
+	 * Opens a forum.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <forum-id>
+	 * : Identifier for the forum.
+	 *
+	 * ## EXAMPLE
+	 *
+	 *    $ wp bp forum open 456
+	 */
+	public function open( $args, $assoc_args ) {
+		// Forum ID.
+		$forum_id = $args[0];
+
+		$user = bbp_open_forum( $forum_id );
+
+		if ( is_numeric( $forum_id ) ) {
+			WP_CLI::success( 'User demoted to the "member" status.' );
+		} else {
+			WP_CLI::error( 'Could not demote the member.' );
+		}
+
+	}
 }
 
-WP_CLI::add_command( 'bbp forums', 'BBPCLI_Forums', array(
+WP_CLI::add_command( 'bbp forum', 'BBPCLI_Forums', array(
 	'before_invoke' => function() {
 		if ( ! class_exists( 'bbPress' ) ) {
 			WP_CLI::error( 'bbPress is not installed or active.' );

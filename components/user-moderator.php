992e2dc3d95030f20cb3c6b77866ed8269410852
Renato Alves <espellcaste@gmail.com>
2017-11-07 09:00:00 -0300

    Users: More users commands added

--- components/user-moderator.php
+++ components/user-moderator.php
@@ -1,20 +1,148 @@
 <?php
 /**
  * Manage bbPress Moderators.
+ *
+ * @since 1.0.0
  */
-class BBPCLI_User_Moderator extends BBPCLI_Component {
+class BBPCLI_User_Moderators extends BBPCLI_Component {
 
+	/**
+	 * Add a moderator
+	 *
+	 * ## OPTIONS
+	 *
+	 * --forum-id=<forum-id>
+	 * : Indentifier of the object (forum).
+	 *
+	 * --user-id=<user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator add --forum-id=545646 --user-id=465456
+	 *    Success: Member added as a moderator.
+	 *
+	 *    $ wp bbp user moderator add --forum-id=465465 --user-id=user_login
+	 *    Success: Member added as a moderator.
+	 *
+	 * @alias create
+	 */
 	public function add( $args, $assoc_args ) {
-		// bbp_add_moderator( $user_id );
+		$forum_id = $assoc_args['forum-id'];
+
+		// Check if forum exists.
+		if ( ! bbp_is_forum( $forum_id ) ) {
+			WP_CLI::error( 'No forum found by that ID.' );
+		}
+
+		$user = $this->get_user_id_from_identifier( $assoc_args['user-id'] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or id' );
+			return;
+		}
+
+		if ( bbp_add_moderator( $forum_id, $user->ID ) ) {
+			WP_CLI::success( 'Member added as a moderator.' );
+		} else {
+			WP_CLI::error( 'Could not add the moderator.' );
+		}
 	}
 
+	/**
+	 * Remove a moderator
+	 *
+	 * ## OPTIONS
+	 *
+	 * --forum-id=<forum-id>
+	 * : Indentifier of the object (forum).
+	 *
+	 * --user-id=<user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator remove --forum-id=456456 --user-id=4995
+	 *    Success: Member removed as a moderator.
+	 *
+	 *    $ wp bbp user moderator remove --forum-id=64654 --user-id=user_login
+	 *    Success: Member removed as a moderator.
+	 *
+	 * @alias delete
+	 */
 	public function remove( $args, $assoc_args ) {
-		// bbp_remove_moderator( $user_id );
+		$forum_id = $assoc_args['forum-id'];
+
+		// Check if forum exists.
+		if ( ! bbp_is_forum( $forum_id ) ) {
+			WP_CLI::error( 'No forum found by that ID.' );
+		}
+
+		$user = $this->get_user_id_from_identifier( $assoc_args['user-id'] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or id' );
+			return;
+		}
+
+		if ( bbp_remove_moderator( $forum_id, $user->ID ) ) {
+			WP_CLI::success( 'Member removed as a moderator.' );
+		} else {
+			WP_CLI::error( 'Could not add the moderator.' );
+		}
 	}
 
-	public function list( $args, $assoc_args ) {
-		// bbp_get_moderators();
+	/**
+	 * List forum moderators
+	 *
+	 * ## OPTIONS
+	 *
+	 * --forum-id=<forum-id>
+	 * : Indentifier of the forum.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 *  ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - ids
+	 *   - json
+	 *   - count
+	 *   - yaml
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator list --forum-id=456456 --format=count
+	 *    5421
+	 *
+	 *    $ wp bbp user moderator list --forum-id=45456 --format=ids
+	 *    5421 454 654654 5454 545
+	 *
+	 * @subcommand list
+	 */
+	public function _list( $_, $assoc_args ) {
+		$formatter = $this->get_formatter( $assoc_args );
+
+		$forum_id = $assoc_args['forum-id'];
+
+		// Check if forum exists.
+		if ( ! bbp_is_forum( $forum_id ) ) {
+			WP_CLI::error( 'No forum found by that ID.' );
+		}
+
+		$mod = bbp_get_moderators( $forum_id );
+		$ids = bbp_get_moderator_ids( $forum_id );
+
+		if ( 'ids' === $formatter->format ) {
+			echo implode( ' ', $ids ); // WPCS: XSS ok.
+		} elseif ( 'count' === $formatter->format ) {
+			$formatter->display_items( count( $ids ) );
+		} else {
+			$formatter->display_items( $mod );
+		}
 	}
 }
 
-WP_CLI::add_command( 'bbp user moderator', 'BBPCLI_User_Moderator' );
+WP_CLI::add_command( 'bbp user moderator', 'BBPCLI_User_Moderators' );
--- components/user.php
+++ components/user.php
@@ -1,12 +1,10 @@
 <?php
 /**
  * Manage bbPress Users.
+ *
+ * @since 1.0.0
  */
 class BBPCLI_Users extends BBPCLI_Component {
-
-	public function url( $args, $assoc_args ) {
-		// $url = bbp_get_user_profile_url( $user_id = 0 );
-	}
 
 	public function topics( $args, $assoc_args ) {
 		$defaults = array(
@@ -24,16 +22,137 @@ class BBPCLI_Users extends BBPCLI_Component {
 		// bbp_get_user_replies_created()
 	}
 
+	/**
+	 * Mark a user's topics and replies as spam
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator spam --user-id=465456
+	 *    Success: User topics and replies marked as spam.
+	 *
+	 *    $ wp bbp user moderator spam --user-id=user_login
+	 *    Success: User topics and replies marked as spam.
+	 *
+	 * @alias unham
+	 */
 	public function spam( $args, $assoc_args ) {
-		// bbp_make_spam_user( $user_id );
+		$user = $this->get_user_id_from_identifier( $args[0] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or id' );
+		}
+
+		if ( bbp_make_spam_user( $user->ID ) ) {
+			WP_CLI::success( 'User topics and replies marked as spam.' );
+		} else {
+			WP_CLI::error( 'Could not mark topics and replies as spam.' );
+		}
 	}
 
+	/**
+	 * Mark a user's topics and replies as ham
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator ham --user-id=465456
+	 *    Success: User topics and replies marked as ham.
+	 *
+	 *    $ wp bbp user moderator ham --user-id=user_login
+	 *    Success: User topics and replies marked as ham.
+	 *
+	 * @alias unspam
+	 */
 	public function ham( $args, $assoc_args ) {
-		// bbp_make_ham_user( $user_id );
+		$user = $this->get_user_id_from_identifier( $args[0] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or id' );
+		}
+
+		if ( bbp_make_ham_user( $user->ID ) ) {
+			WP_CLI::success( 'User topics and replies marked as ham.' );
+		} else {
+			WP_CLI::error( 'Could not mark topics and replies as ham.' );
+		}
 	}
 
-	public function update_role( $args, $assoc_args ) {
-		// bbp_set_user_role( $user_id, $new_role );
+	/**
+	 * Set user role
+	 *
+	 * ## OPTIONS
+	 *
+	 * --user-id<user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * --role<role>
+	 * : Role to set for the member.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator set_role --user-id=465456 --role=mod
+	 *    Success: User topics and replies marked as ham.
+	 *
+	 *    $ wp bbp user moderator set_role --user-id=user_login --role=mod
+	 *    Success: User topics and replies marked as ham.
+	 */
+	public function set_role( $args, $assoc_args ) {
+		$user = $this->get_user_id_from_identifier( $assoc_args['user-id'] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or id' );
+		}
+
+		$retval = bbp_set_user_role( $user->ID, $assoc_args['role'] );
+
+		if ( is_string( $retval ) ) {
+			WP_CLI::success( sprintf( 'New role for user set: %s', $retval ) );
+		} else {
+			WP_CLI::error( 'Could not set new role for user.' );
+		}
+	}
+
+	/**
+	 * Get URL of the user profile page
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    $ wp bbp user moderator permalink --user-id=465456
+	 *    Success: User profile page: https://site.com/
+	 *
+	 *    $ wp bbp user moderator url --user-id=user_login
+	 *    Success: User profile page: https://site.com/
+	 *
+	 * @alias url
+	 */
+	public function permalink( $args, $assoc_args ) {
+		$user = $this->get_user_id_from_identifier( $args[0] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or id' );
+		}
+
+		$permalink = bbp_get_user_profile_url( $user->ID );
+
+		if ( is_string( $permalink ) ) {
+			WP_CLI::success( sprintf( 'User profile page: %s', $permalink ) );
+		} else {
+			WP_CLI::error( 'Could not find user profile page.' );
+		}
 	}
 }
 
